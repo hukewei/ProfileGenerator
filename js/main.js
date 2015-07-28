49ba34e1a274e44ecce0068164ee5a4b6c3a7551
unknown <keweihu@yahoo.com>
2015-07-28 16:11:11 +0200

    ADD::disruption function and new profile

--- js/main.js
+++ js/main.js
@@ -2,16 +2,15 @@ function onData(data)
 {
     var selected_date = $('#my_date_picker').datepicker('getDate');
     var julian_day = selected_date.getJulian();
-    var timestamp = selected_date.getTime()/1000 + 20 * 3600;
-    console.log(selected_date.getJulian());
-    console.log(timestamp);
+    var timestamp = Math.round(selected_date.getTime()/1000) + 20 * 3600;
     var replaced_value = data.replace("##JULIAN_DAY##", julian_day);
     replaced_value = replaced_value.replace("##TIMESTAMP##", timestamp);
     if($('#option1').prop('checked')) {
         //no disruption, remove disruption
         //console.log(replaced_value);
-        replaced_value = replaced_value.replace(/[\r\n]/g, "@NEWLINE@");
+        replaced_value = replaced_value.replace(/\r\n/g, "@NEWLINE@");
         replaced_value = replaced_value.replace(/\"DisruptionHistory(.*?)IdentityElement/g, "\"IdentityElement");
+        replaced_value = replaced_value.replace(/\,@NEWLINE@\s\s+\"Communications(.*?)\]/g, "");
         replaced_value = replaced_value.replace(/@NEWLINE@/g, "\n");
     }
     //clear the previous text
@@ -28,12 +27,12 @@ function parsingProfile(id)
             }, 'text');
             break;
         case 2:
-            $.get('raw/isabelle_dubois.json', function(data) { 
+            $.get('raw/bruce_wayne.json', function(data) { 
                 onData(data);
             }, 'text');
             break;
         case 3:
-            $.get('raw/isabelle_dubois.json', function(data) { 
+            $.get('raw/kewei_hu.json', function(data) { 
                 onData(data);
             }, 'text');
             break;
@@ -53,7 +52,12 @@ $(document).ready( function() {
     $( "#hidden_block").hide();
     $( "#hidden_info").hide();
     $('#my_date_picker').datepicker({
+        autoclose: true,
+        todayBtn: true,
+        todayHighlight: true,
+        orientation: "bottom" 
     });
+    $("#my_date_picker").datepicker("setDate", new Date());
     $.get('raw/isabelle_dubois.json', function(data) {    
     }, 'text');
     //Dropdown plugin data
@@ -78,13 +82,6 @@ $(document).ready( function() {
             selected: false,
             description: "Pick me!",
             imageSrc: "img/kewei_hu.png"
-        },
-        {
-            text: "Anonymous",
-            value: 4,
-            selected: false,
-            description: "Unknown",
-            imageSrc: "img/anonymous.png"
         }
     ];
 
@@ -94,7 +91,6 @@ $(document).ready( function() {
     selectText: "Select your profile to generate",
     imagePosition:"left",
     onSelected: function(selectedData){
-        console.log(selectedData);
         selected_profile = selectedData.selectedData.value;
     }   
     });
@@ -114,7 +110,8 @@ $(document).ready( function() {
         if($('#my_date_picker').datepicker('getDate') != undefined) {
             parsingProfile(selected_profile);
             $( "#hidden_info").hide();
-            $( "#hidden_block").show();
+            $( "#hidden_block").fadeOut();
+            $( "#hidden_block").fadeIn();
         } else {
             $( "#hidden_info").show();
             $( "#hidden_block").hide();
