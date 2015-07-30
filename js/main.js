9d9f0dfbe8855a5c01b7cec795e1043573374f5c
unknown <keweihu@yahoo.com>
2015-07-30 10:56:41 +0200

    FIX::TIMESTAMP replace

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 function onData(data) 
 {
     var selected_date = $('#my_date_picker').datepicker('getDate');
-    var julian_day = selected_date.getJulian();
+    var julian_day = selected_date.getJulian() + 1;
     var timestamp = Math.round(selected_date.getTime()/1000) + 20 * 3600;
     var replaced_value = data.replace("##JULIAN_DAY##", julian_day);
-    replaced_value = replaced_value.replace("##TIMESTAMP##", timestamp);
+    replaced_value = replaced_value.replace(/##TIMESTAMP##/g, timestamp);
     if($('#option1').prop('checked')) {
         //no disruption, remove disruption
         //console.log(replaced_value);
         replaced_value = replaced_value.replace(/\r\n/g, "@NEWLINE@");
+        //remove DisruptionHistory
         replaced_value = replaced_value.replace(/\"DisruptionHistory(.*?)IdentityElement/g, "\"IdentityElement");
+        //remove Communication
         replaced_value = replaced_value.replace(/\,@NEWLINE@\s\s+\"Communications(.*?)\]/g, "");
         replaced_value = replaced_value.replace(/@NEWLINE@/g, "\n");
+    } else {
+        //remove extra newlines
+        replaced_value = replaced_value.replace(/\r\n/g, "@NEWLINE@");
+        replaced_value = replaced_value.replace(/@NEWLINE@/g, "\n");
     }
+
     //clear the previous text
     $( "#textToCopy" ).text('');
     $( "#textToCopy" ).append(replaced_value);
